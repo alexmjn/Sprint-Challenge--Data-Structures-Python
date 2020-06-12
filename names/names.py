88ad9faa16862a3dc9b23dcc6fbf600cce28f6bf
alexmjn <a.jenkneary@gmail.com>
2020-06-12 11:29:53 -0400

    Names.py

--- names/names.py
+++ names/names.py
@@ -22,6 +22,57 @@ end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
+class BinarySearchTree:
+    """Because the names are sortable, we can use a binary search tree to
+    reduce the runtime from O(n^2) -- traversing all of file 2 for each element
+    in file 1 -- to O(n log n), where each traversal takes log(n) time
+    (the runtime of a binary search tree).
+    """
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left == None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right == None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
+    def contains(self, target):
+        if self.value == target:
+            return True
+        elif target < self.value:
+            if self.left is not None:
+                return self.left.contains(target)
+            else:
+                return False
+        elif target > self.value:
+            if self.right is not None:
+                return self.right.contains(target)
+            else:
+                return False
+
+
+start_time = time.time()
+duplicates = []
+binary_search_tree = BinarySearchTree(names_1[0])
+for name_1 in names_1[1:]:
+    binary_search_tree.insert(name_1)
+
+for name_2 in names_2:
+    if binary_search_tree.contains(name_2):
+        duplicates.append(name_2)
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
+
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
